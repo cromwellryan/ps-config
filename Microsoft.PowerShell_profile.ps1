5f3c0a3f6b07bb21d05ee597f01f29f13d16012a
Ryan Cromwell <ryan@echelontouch.com>
2012-05-21 13:47:51 -0400

    organized profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,33 +3,23 @@ $scriptRoot = Split-Path (Resolve-Path $myInvocation.MyCommand.Path)
 Import-Module Psget
 Import-Module PsGist
 
-# TFS Helpers
+##############################
+# Team Founation Server (TFS)
+##############################
 function GoToProjects() { cd ~/projects }
-function TfsStatus() { tf status . /r }
-function TfsUpdate() { tf get . /r }
-function New-PSWindow { Invoke-item "$pshome\powershell.exe" }
-
-set-alias nw new-pswindow
 set-alias gop GoToProjects
-set-alias ts TfsStatus
-set-alias tu TfsUpdate
 
-# Git helpers
-set-content Function:\ga "git add `$args"
+set-content Function:\ts "tf status . /r"
+set-content Function:\tu "tf get . /r"
 
-set-content Function:\pd "pushd ."
+# Visual Studio VsVars include (run vsvars32
+. (join-path $scriptRoot "/vsvars.ps1")
 
-$env:Path += ";" + $env:USERPROFILE + "\AppData\Roaming\npm\node_modules\"
-
-function curlex($url, $filename) {
-	$path = [io.path]::gettemppath() + "\" + $filename
-
-	if( test-path $path ) { rm -force $path }
-
-	(new-object net.webclient).DownloadFile($url, $path)
-
-	return new-object io.fileinfo $path
-}
+##############################
+# Powershell
+##############################
+function New-PSWindow { Invoke-item "$pshome\powershell.exe" }
+set-alias nw new-pswindow
 
 function CreateDirectoryAndGo( $name ) {
 	$dirinfo = mkdir $name
@@ -37,24 +27,36 @@ function CreateDirectoryAndGo( $name ) {
 }
 set-alias mkcd CreateDirectoryAndGo
 
-function start-host() {
-	start 'iisexpress' @('/path:' + (pwd).Path)
-}
+function get-path() { $env:Path.Split(';') }
 
-function get-path() { ls ENV: | where {$_.Name -eq "PATH"} | % { $_.Value.Split(';')} }
-
+set-content Function:\pd "pushd ."
 set-content Function:\mklink "cmd /c mklink `$args"
 set-content Function:\v "vim `$args"
 
 # Allow local unsigned scripts, but remote scripts must be signed
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
+##############################
+# Git
+##############################
+. (join-path $scriptRoot "/git.ps1")
+set-content Function:\ga "git add `$args"
+set-content Function:\gcm "git cm `$args"
+
 # Load posh-git example profile
 . (join-path $scriptRoot "/Modules/posh-git/profile.example.ps1")
 . (join-path $scriptRoot "/screen.ps1")
 
-# Visual Studio VsVars include (run vsvars32
-. (join-path $scriptRoot "/vsvars.ps1")
+
+##############################
+# Node
+##############################
+
+# Put globally installed npm packages in PATH
+$env:Path += ";" + $env:USERPROFILE + "\AppData\Roaming\npm\node_modules\"
+
+function start-host() {
+	start 'iisexpress' @('/path:' + (pwd).Path)
+}
 
 . (join-path $scriptRoot "/MountISO.ps1")
-. (join-path $scriptRoot "/git.ps1")
