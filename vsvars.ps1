b90568c95d419bd2ba0f6e1b25d8b09261eade2b
Ryan Cromwell <ryan@echelontouch.com>
2013-03-29 11:09:08 -0400

    Allow for VSVars32 11 or VSVars32 '11.0' where before it failed on numbers alone

--- vsvars.ps1
+++ vsvars.ps1
@@ -8,12 +8,17 @@ function Get-Batchfile ($file) {
   
 function VsVars32($version = "10.0")
 {
-    $key = "HKLM:SOFTWARE\Microsoft\VisualStudio\" + $version
+    $versionkey = $version
+    if( $version -is [int] ) {
+      $versionkey = $version.ToString("00.0")
+    }
+
+    $key = "HKLM:SOFTWARE\Microsoft\VisualStudio\" + $versionkey
     $VsKey = get-ItemProperty $key
     $VsInstallPath = [System.IO.Path]::GetDirectoryName($VsKey.InstallDir)
     $VsToolsDir = [System.IO.Path]::GetDirectoryName($VsInstallPath)
     $VsToolsDir = [System.IO.Path]::Combine($VsToolsDir, "Tools")
     $BatchFile = [System.IO.Path]::Combine($VsToolsDir, "vsvars32.bat")
     Get-Batchfile $BatchFile
-    [System.Console]::Title = "Visual Studio " + $version + " Windows Powershell"
+    [System.Console]::Title = "Visual Studio " + $versionkey + " Windows Powershell"
 }
