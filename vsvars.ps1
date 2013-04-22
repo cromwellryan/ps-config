57b201eb553da20c926ddf6ef90af62b1bad6aeb
Ryan Cromwell <ryan@echelontouch.com>
2013-04-22 10:58:41 -0400

    vsvars32 now works for 2012. input is 10 or 11 as integers.  vsvars32 is vsdevcmd in 2012

--- vsvars.ps1
+++ vsvars.ps1
@@ -1,24 +1,29 @@
 function Get-Batchfile ($file) {
     $cmd = "`"$file`" & set"
-    cmd /c $cmd | Foreach-Object {
+    cmd /c "$cmd" | Foreach-Object {
         $p, $v = $_.split('=')
         Set-Item -path env:$p -value $v
     }
 }
   
-function VsVars32($version = "10.0")
+function VsVars32([int]$version = 10)
 {
-    $versionkey = $version
-    if( $version -is [int] ) {
-      $versionkey = $version.ToString("00.0")
+    $versionkey = $version.ToString("00.0")
+
+    $batfilename = "vsvars32.bat" 
+
+    if($version -eq 11) {
+      $batfilename = "vsdevcmd.bat"
     }
+
+
 
     $key = "HKLM:SOFTWARE\Microsoft\VisualStudio\" + $versionkey
     $VsKey = get-ItemProperty $key
     $VsInstallPath = [System.IO.Path]::GetDirectoryName($VsKey.InstallDir)
     $VsToolsDir = [System.IO.Path]::GetDirectoryName($VsInstallPath)
     $VsToolsDir = [System.IO.Path]::Combine($VsToolsDir, "Tools")
-    $BatchFile = [System.IO.Path]::Combine($VsToolsDir, "vsvars32.bat")
-    Get-Batchfile $BatchFile
+    $BatchPath = join-path $VsToolsDir $batfilename
+    Get-Batchfile $BatchPath
     [System.Console]::Title = "Visual Studio " + $versionkey + " Windows Powershell"
 }
