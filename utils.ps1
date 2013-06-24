bab2ccaec6ccb89f1f0ee2f891706f449f61789c
Ryan Cromwell <ryan@echelontouch.com>
2013-06-24 11:26:40 -0400

    Added First/Select-First and cleaned Test-Any indents

--- utils.ps1
+++ utils.ps1
@@ -1,8 +1,18 @@
+function Select-First() {
+  begin { $result = $null }
+  process { 
+    if( $result -eq $null ) {
+      $result = $_ 
+    }
+  }
+  end { return $result }
+}
+set-alias first Select-First
 
 function Test-Any { 
-    begin { $result = $false }
-      process { $result = $true }
-        end { $result }
+  begin { $result = $false }
+  process { $result = $true }
+  end { $result }
 }
 set-alias any Test-Any
 
